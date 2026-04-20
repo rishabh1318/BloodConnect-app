2216759680813594174bbf0f631c396d059cc89b
Rishabh Kushwah <rishabhsingh2380@gmail.com>
2026-04-20 16:11:35 +0530

    Update base path in Vite config

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/BloodConnect-app/'  // <-- add your repo name here
+  base: '/BloodLINK-app/'  // <-- add your repo name here
 });
